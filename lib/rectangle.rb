2d1c776c1ae4396507823f714aee2457c54d465e
Ashley <a.clarkem@gmail.com>
2014-07-30 10:13:04 -0600

    refactor

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 class Rectangle
+  attr_accessor(:length, :width)
 
-  def initialize
-
+  def initialize(length,width)
+    @length = length
+    @width = width
   end
 
-  def area (length, width)
+  def area
     length * width
   end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,27 +1,21 @@
 require 'spec_helper'
 
 describe Rectangle do
-  before :each do
-    @rectangle = Rectangle.new
+  before :all do
+    @rectangle = Rectangle.new(2,3)
   end
 
   describe "new rectangle" do
     it "takes two parameters and returns a Rectangle object" do
-      rectangle = Rectangle.new
-      expect(rectangle).to be_an_instance_of(Rectangle)
+      expect(@rectangle).to be_an_instance_of(Rectangle)
     end
-  end
-
-  describe "rectangle can return its area" do
     it "multiplies length by width" do
-    rectangle = Rectangle.new
-    expect(rectangle.area(2,3)).to eq(6)
+      expect(@rectangle.area).to eq(6)
     end
   end
 end
 
- rectangle = Rectangle.new
-p rectangle.area(2,3)
+
 
 
 
